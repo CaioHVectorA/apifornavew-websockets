243be745651084d277c7c0d0030f691e36de8e24
CaioHVectorA <caihebatista@gmail.com>
2023-03-01 18:50:22 -0300

    conexão com banco de dados do mongoDB. e inicialização do ambiente para rotas

---
+++ Routes.js
@@ -0,0 +1,3 @@
+const express = require('express')
+const routes = express.Router()
+const mongoose = require('mongoose')
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const routes = require('./Routes')
+const app = express()
+const port = 4000||process.env.port
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors())
+
+// iRt9PsVNkKe3DOpk
+mongoose.set('strictQuery',false)
+
+mongoose.connect('mongodb+srv://APIforNAVE:<password>@dados.dbbl6x2.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+    app.listen(port, () => console.log('servidor Rodando!'))
+})
+.catch(err => console.log(err))
+
+module.exports = app
